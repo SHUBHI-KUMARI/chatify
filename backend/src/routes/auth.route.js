3eb76805808443bbdbe65265a0339741f674aadc
SHUBHI-KUMARI <shubhimit21@gmail.com>
2025-11-25 01:22:38 +0530

    signup controller added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,9 @@
 import express from "express";
+import { Signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.send("Signup endpoint");
-});
+router.post("/signup", Signup);
 
 router.get("/login", (req, res) => {
   res.send("Login endpoint");
